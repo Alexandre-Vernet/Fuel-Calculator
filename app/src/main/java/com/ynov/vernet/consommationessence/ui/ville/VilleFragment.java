fc265f2a378243246daf68e6fd6bd993c980c610
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-11-04 15:54:37 +0100

    Vérifier au moment du clic du bouton que la zone de texte n'est pas vide

--- app/src/main/java/com/ynov/vernet/consommationessence/ui/ville/VilleFragment.java
+++ app/src/main/java/com/ynov/vernet/consommationessence/ui/ville/VilleFragment.java
@@ -1,6 +1,7 @@
 package com.ynov.vernet.consommationessence.ui.ville;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,28 +38,34 @@ public class VilleFragment extends Fragment {
 
         // Au clic du bouton valider
         view.findViewById(R.id.btnValider).setOnClickListener(v -> {
+            // Si la zone est vide
+            if (editTextDistance.getText().toString().isEmpty()) {
+                editTextDistance.setError("Erreur");
+                new Handler().postDelayed(() -> {
+                    editTextDistance.setError(null);
+                }, 2000);
+            } else {
+                // Récupérer la valeur de la zone de texte en réel
+                String value = editTextDistance.getText().toString();
+                double finalValue = Integer.parseInt(value);
 
-            // Récupérer la valeur de la zone de texte en réel
-            String value = editTextDistance.getText().toString();
-            double finalValue = Integer.parseInt(value);
+                // Calcul du prix
+                double prix = finalValue * 1.5 / 12.82;
+                prix = Math.floor(prix * 100) / 100;                    /*2 chiffres après la virgule*/
 
-            // Calcul du prix
-            double prix = finalValue * 1.5 / 12.82;
-            prix = Math.floor(prix * 100) / 100;                    /*2 chiffres après la virgule*/
+                // Calcul de la consommation
+                double consommation = finalValue * 1 / 12.82;
+                consommation = Math.floor(consommation * 100) / 100;    /*2 chiffres après la virgule*/
 
-            // Calcul de la consommation
-            double consommation = finalValue * 1 / 12.82;
-            consommation = Math.floor(consommation * 100) / 100;    /*2 chiffres après la virgule*/
-
-            // Afficher le résultat
-            new AlertDialog.Builder(getContext())
-                    .setIcon(android.R.drawable.ic_dialog_info)
-                    .setTitle("Calculateur")
-                    .setMessage("Votre trajet coûtera " + prix + " € \n Vous consommerez " + consommation + " L")
-                    .setPositiveButton("Ok", (dialogInterface, i) -> {
-                    })
-                    .show();
+                // Afficher le résultat
+                new AlertDialog.Builder(getContext())
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .setTitle("Calculateur")
+                        .setMessage("Votre trajet coûtera " + prix + " € \n Vous consommerez " + consommation + " L")
+                        .setPositiveButton("Ok", (dialogInterface, i) -> {
+                        })
+                        .show();
+            }
         });
-
     }
 }
