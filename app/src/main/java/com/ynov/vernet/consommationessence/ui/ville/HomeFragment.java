e3fde2e2e3e90678a0dfeecbceb193d817fb29a7
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-11-04 14:34:30 +0100

    Calcul du prix à payer pour le trajet

--- app/src/main/java/com/ynov/vernet/consommationessence/ui/ville/HomeFragment.java
+++ app/src/main/java/com/ynov/vernet/consommationessence/ui/ville/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,8 @@ import com.ynov.vernet.consommationessence.R;
 
 public class HomeFragment extends Fragment {
 
+    EditText editTextDistance;
+
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -22,6 +25,7 @@ public class HomeFragment extends Fragment {
 
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_ville, container, false);
+
         return root;
     }
 
@@ -29,16 +33,24 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        editTextDistance = getView().findViewById(R.id.editTextDistance);
+
+
         // Au clic du bouton valider
         view.findViewById(R.id.btnValider).setOnClickListener(v -> {
 
+            // Récupérer la valeur de la zone de texte en réel
+            String value = editTextDistance.getText().toString();
+            double finalValue = Integer.parseInt(value);
+
             // Calcul de la consommation
+            double prix = finalValue * 1.5 / 12.82;
 
             // Afficher le résultat
             new AlertDialog.Builder(getContext())
                     .setIcon(android.R.drawable.ic_dialog_info)
                     .setTitle("Titre")
-                    .setMessage("Message")
+                    .setMessage("Votre trajet coûtera " + prix + " €")
                     .setPositiveButton("Ok", (dialogInterface, i) -> {
                     })
                     .show();
